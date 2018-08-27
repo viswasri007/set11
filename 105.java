817edb5be1e5e05c0a33195d6921cdfe42fafe1a
Viswa Sunny <viswasunny007@gmail.com>
2018-08-27 10:22:09 +0530

    Create 105.java

---
+++ 105.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		System.out.print(a+""+b);
+	}
+}
