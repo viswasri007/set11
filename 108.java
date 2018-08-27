ac509e4a80af7d53f9081fec3fe1d7339760dcec
Viswa Sunny <viswasunny007@gmail.com>
2018-08-27 10:47:13 +0530

    Create 108.java

---
+++ 108.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int k=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=s.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(a[i]>a[j])
+				{
+					int t=a[i];
+					a[i]=a[j];
+					a[j]=t;
+				}
+			}
+		}
+		System.out.print(a[k-1]);
+	}
+}
